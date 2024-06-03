46f884e28efcd0029dd608fcf0f6b4377daf42d7
miri <mk6764576@gmail.com>
2024-06-04 00:33:52 +0300

    With client

--- TinyURL-Server/models/Link.js
+++ TinyURL-Server/models/Link.js
@@ -1,15 +1,4 @@
 
-// const linkSchema = new mongoose.Schema({
-//   originalUrl: { type: String, required: true },
-//   clicks: [{
-//     insertedAt: { type: Date, default: Date.now },
-//     ipAddress: { type: String, required: true }
-//   }]
-// });
-
-// const Link = mongoose.model('Link', linkSchema);
-
-// export default Link;
 import mongoose from 'mongoose';
 
 const linkSchema = new mongoose.Schema({
